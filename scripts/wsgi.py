fe4149f37e10fc6cca1c861fae0b04342e033647
Kevin Ferneding <yourbro17@gmail.com>
2025-04-16 16:14:06 +0200

    changed port

--- scripts/wsgi.py
+++ scripts/wsgi.py
@@ -16,7 +16,7 @@ import redis
 script_path = Path(__file__).parent.resolve()
 root_path = script_path.parent.resolve()
 app = Flask(__name__, template_folder=os.path.join(root_path, 'webinterface'))
-redis_client = redis.Redis(host='localhost', port=16379, db=0)
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 limiter = Limiter(app=app, key_func=get_remote_address, storage_uri="redis://localhost:16379",default_limits=["200 per day", "50 per hour"])
 
 # Queue to hold our output
